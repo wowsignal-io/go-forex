dfa849cc8dd711368a06ff64cfc9c2e99bd8519e
Adam Sindelar <adam@wowsignal.io>
2024-05-11 12:17:48 +0200

    Add script to refresh offline rates
    Also, refresh ECB offline rates.

--- forex/offline/update.sh
+++ forex/offline/update.sh
@@ -1,7 +1,20 @@
-The file 'eurofxref-hist.zip' was obtained from the website of the European
-Central Bank (ECB) on 2024-05-11 using the URL
-https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist.zip.
+#!/bin/bash
 
+# set -e
+
+BOC_URL='https://www.bankofcanada.ca/valet/observations/group/FX_RATES_DAILY/csv?start_date=2017-01-03'
+ECB_URL='https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist.zip'
+
+ECB_FILENAME='eurofxref-hist.zip'
+BOC_FILENAME='boc_rates.csv'
+
+read -r -d '' ECB_LICENSE_HEADER << EOF || true
+The file 'eurofxref-hist.zip' was obtained from the website of the European
+Central Bank (ECB) on $(date +%Y-%m-%d) using the URL
+${ECB_URL}.
+EOF
+
+read -r -d '' ECB_LICENSE_TEXT << EOF || true
 The rest of this file is an excerpt from ECB's copyright policy retrieved on
 2022-02-10 from https://www.ecb.europa.eu/services/disclaimer/html/index.en.html
 
@@ -34,3 +47,35 @@ or in part, of documents that bear the name of their authors, such as ECB
 Working Papers and ECB Occasional Papers, in the form of a different publication
 (whether printed or produced electronically) is permitted only with the explicit
 prior written authorisation of the ECB or the authors.
+EOF
+
+function script_dir() {
+    local relative_dir
+    relative_dir="$(dirname "${BASH_SOURCE}")"
+    pushd "${relative_dir}" > /dev/null 2>&1
+    pwd
+    popd > /dev/null 2>&1
+}
+
+function die() {
+    [[ -n "${2}" ]] && echo "${2}" >&2
+    [[ -n "${1}" ]] && exit "${1}"
+    exit 255
+}
+
+pushd "$(script_dir)" > /dev/null
+
+# Download the files:
+
+curl -L "${ECB_URL}" > "${ECB_FILENAME}"
+
+# Update the license:
+
+{
+    echo "${ECB_LICENSE_HEADER}"
+    echo
+    echo "${ECB_LICENSE_TEXT}"
+} > ./LICENSE || die
+
+
+popd > /dev/null
